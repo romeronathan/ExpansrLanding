fc599465fb246bd869d98869cb3f2965e7aa7fc7
Nathan Romero <nathan.romero01@stclairconnect.ca>
2022-08-08 17:06:50 -0400

    Logo Removal

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -40,9 +40,7 @@ const Navbar = () => {
                     </li>
                 </ul >
                 <div className="flex flex-col content-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="m-auto h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
-                    </svg>
+
                     <a>expansr</a>
                 </div>
 
